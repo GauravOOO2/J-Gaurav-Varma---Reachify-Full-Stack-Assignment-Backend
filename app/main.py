674879bfdf72f7e15952f594e9ea5f76befdcf58
Gaurav0002 <gauravking918@gmail.com>
2024-11-08 00:25:06 +0530

    Dploy 4

--- app/main.py
+++ app/main.py
@@ -19,8 +19,7 @@ app = FastAPI(title="TODO API with Auth")
 
 # CORS configuration
 origins = [
-    "https://j-gaurav-varma-reachify-full-stack-assignment-frontend.vercel.app/",
-    "https://j-gaurav-varma-reachify-full-stack-assignment-backend.vercel.app/register"  # Your frontend URL
+    "https://j-gaurav-varma-reachify-full-stack-assignment-frontend.vercel.app",  # Your frontend URL
     # Add other origins if needed
 ]
 
